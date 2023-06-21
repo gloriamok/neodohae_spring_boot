a24ec81d5350eac07a119cf497c0f9d65d3ac160
gloriamok <gloriamok15@gmail.com>
2023-06-21 16:35:48 +0900

    Initail Setting

---
+++ neodohae_spring_boot/src/test/java/com/neodohae_spring_boot/neodohae_spring_boot/NeodohaeSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neodohae_spring_boot.neodohae_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeodohaeSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
