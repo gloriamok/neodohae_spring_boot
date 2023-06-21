47c1d3ed515f1857e021547c9f5b005f360a8af8
gloriamok <gloriamok15@gmail.com>
2023-06-21 17:47:07 +0900

    initial setting

---
+++ src/main/java/com/neodohae_spring_boot/neodohae_spring_boot/NeodohaeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.neodohae_spring_boot.neodohae_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeodohaeSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeodohaeSpringBootApplication.class, args);
+	}
+
+}
